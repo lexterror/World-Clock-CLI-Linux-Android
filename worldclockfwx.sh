8ed57ac8107820ee1be0ede126a7033c154944e1
Alex Terranova <alexterranova81@gmail.com>
2024-03-07 02:16:50 -0500

    Add files via upload

--- worldclockfwx.sh
+++ worldclockfwx.sh
@@ -12,10 +12,10 @@ while true; do
     printf "\033c"
     clear
   
-    echo 'WorldClock 1.0 |Airport Edition|'
+    echo 'WorldClock 1.0 | Airport Edition |'
     echo ''
     echo "---------------------------------------------"
-    echo -e "|${rw}Zone   |${rw}Date              |${rw}Time      |${rw}${rw}Loc  |"
+    echo -e "|${rw} Zone  |${rw} Date             |${rw} Time     |${rw}${rw} Loc |"
     echo "---------------------------------------------"
     for tz in  Pacific/Pago_Pago Pacific/Honolulu America/Yakutat US/Pacific America/Denver US/Central US/Eastern Brazil/West America/Recife America/Godthab Atlantic/Azores Europe/London Europe/Madrid Asia/Jerusalem Europe/Moscow Asia/Baku Asia/Karachi Asia/Dhaka Asia/Bangkok Asia/Hong_Kong Asia/Tokyo Pacific/Guam Australia/Sydney Pacific/Fiji; do
     echo -e "${rw}| ${rr}$(TZ=$tz date '+%z') ${rw}| ${lo}$(TZ=$tz date '+%a %d-%b, %Y') ${rw}| ${lg}$(TZ=$tz date '+%I:%M %p') ${rw}| ${lb}${airports[$var]} ${rw}|"
